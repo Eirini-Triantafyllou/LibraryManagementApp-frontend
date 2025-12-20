3d77f36a3eb93f38b5c5f06d9871fd699a02deb8
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-20 12:31:55 +0200

    Add app.ts and app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,14 @@ import { Step3 } from './components/step3/step3';
 import { Step4EventBind } from './components/step4-event-bind/step4-event-bind';
 
 import { Navbar } from './components/navbar/navbar';
-import { ListGroupMenu } from './components/list-group-menu/list-group-menu';
+import { Welcome } from './components/welcome/welcome';
+import { Footer } from './components/footer/footer';
+// import { ListGroupMenu } from './components/list-group-menu/list-group-menu';
+// import { ReaderDashboard } from './components/reader-dashboard/reader-dashboard';
+// import { LibrarianDashboard } from './components/dashboards/librarian-dashboard/librarian-dashboard';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Person } from './shared/interfaces/person';
+import { PersonReactiveForms } from "./components/step9-reactive-forms/person-reactive-forms/person-reactive-forms";
 
 
 @Component({
@@ -16,12 +21,17 @@ import { Person } from './shared/interfaces/person';
     RouterLink,
     RouterOutlet,
     Navbar,
-    ListGroupMenu,
-    Step1, 
-    Step2, 
-    Step3, 
-    Step4EventBind
-  ],
+    // ListGroupMenu,
+    // ReaderDashboard,
+    // LibrarianDashboard,
+    Welcome,
+    Footer,
+    Step1,
+    Step2,
+    Step3,
+    Step4EventBind,
+    PersonReactiveForms
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
