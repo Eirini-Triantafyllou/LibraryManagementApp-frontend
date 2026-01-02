40c301f52ba70f9d209b0ea6e42d6c63343ac667
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-02 11:14:10 +0200

    fix: update reader dashboard

--- src/app/components/reader-dashboard/reader-dashboard.ts
+++ src/app/components/reader-dashboard/reader-dashboard.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-reader-dashboard',
+  selector: 'app-reader',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './reader-dashboard.html',
   styleUrl: './reader-dashboard.css',
