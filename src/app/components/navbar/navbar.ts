0e929072e65f41ecd4ccd77b9cc808f90c569ba3
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-02 12:04:54 +0200

    inactivate navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,20 +1,20 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { UserService } from '../../shared/services/user.service';
+// import { Component, inject } from '@angular/core';
+// import { RouterLink } from "@angular/router";
+// import { UserService } from '../../shared/services/user.service';
 
-@Component({
-  selector: 'app-navbar',
-  imports: [RouterLink],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
-})
-export class Navbar {
-  userService = inject(UserService);
+// @Component({
+//   selector: 'app-navbar',
+//   imports: [RouterLink],
+//   templateUrl: './navbar.html',
+//   styleUrl: './navbar.css',
+// })
+// export class Navbar {
+//   userService = inject(UserService);
 
-  user = this.userService.user;
+//   user = this.userService.user;
 
-  logout(){
-    this.userService.logoutUser();
-  }
+//   logout(){
+//     this.userService.logoutUser();
+//   }
 
-}
+// }
