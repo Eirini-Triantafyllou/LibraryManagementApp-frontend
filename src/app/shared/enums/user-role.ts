7579c641c82d0c9f75a129c4707db6cc4e553b09
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-02 11:11:23 +0200

    feat: create userRole enum

---
+++ src/app/shared/enums/user-role.ts
@@ -0,0 +1,10 @@
+export enum UserRole {
+  // READER = "Reader",
+  // ADMIN = "Admin",
+  // LIBRARIAN = "Librarian" // string
+
+  Reader = 0,
+  Librarian = 1,
+  Admin = 2                  // numeric για να συμβαδίζει με το swagger 
+}
+
