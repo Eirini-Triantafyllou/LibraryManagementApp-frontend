d42406caf0cc3ecfc663e50bb0954a1725af4413
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-03 17:12:55 +0200

    fix: update dashboard service

--- src/app/shared/services/dashboard.service.ts
+++ src/app/shared/services/dashboard.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { UserRole } from '../enums/user-role';
+import { UserReadOnlyDTO } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,30 @@ import { UserRole } from '../enums/user-role';
 export class DashboardService {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+ 
+  getUserRoleString(): string {
+  const user = this.authService.currentUser();
+  
+  if (!user || user.userRole == null) {
+    return '';
+  }
+  
+  const role = user.userRole;
+  
+  if (typeof role === 'number') {
+    if (role === 0) return 'Reader';
+    if (role === 1) return 'Librarian';
+    if (role === 2) return 'Admin';
+
+  } else if (typeof role === 'string') {
+     if (role === 'Reader') return 'Reader';
+    if (role === 'Librarian') return 'Librarian';
+    if (role === 'Admin') return 'Admin';
+  }
+
+  return 'Reader'; // Default
+}
 
   // Παίρνει το σωστό dashboard path για τον τρέχοντα χρήστη
   getUserDashboardPath(): string {
@@ -20,21 +45,35 @@ export class DashboardService {
 
     switch(user.userRole) {
       case UserRole.Reader:
-        return '/reader/dashboard';
+        return '/reader-dashboard';
       case UserRole.Librarian:
-        return '/librarian/dashboard';
+        return '/librarian-dashboard';
       case UserRole.Admin:
-        return '/admin/dashboard';
+        return '/admin-dashboard';
       default:
         return '/unauthorized';
     }
   }
 
-  // Κάνει redirect στον σωστό dashboard
-  redirectToUserDashboard(): void {
-    const path = this.getUserDashboardPath();
-    this.router.navigate([path]);
+  // Κάνει redirect στο σωστό dashboard
+  redirectToUserDashboard(user?: UserReadOnlyDTO): void {
+    const role = user?.userRole?.toString() || 'Reader';
+    
+    switch(role) {
+      case 'Admin':
+        this.router.navigate(['/admin-dashboard']);
+        break;
+      case 'Librarian':
+        this.router.navigate(['/librarian-dashboard']);
+        break;
+      case 'Reader':
+        this.router.navigate(['/reader-dashboard']);
+        break; 
+      default:
+        this.router.navigate(['/reader-dashboard']);
+    }
   }
+
 
   /**
    * Επιστρέφει το dashboard component ανάλογα με το role
