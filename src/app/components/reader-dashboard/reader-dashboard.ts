827468d37420d8b28e8a8d3ff2d2bdab773a2050
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-04 18:34:05 +0200

    fix: update reader dashboard

--- src/app/components/reader-dashboard/reader-dashboard.ts
+++ src/app/components/reader-dashboard/reader-dashboard.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
+import { UpdateUser } from '../update-user/update-user';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-reader',
-  imports: [RouterLink, RouterLinkActive],
+  selector: 'app-reader-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, Navbar, UpdateUser, Footer],
   templateUrl: './reader-dashboard.html',
   styleUrl: './reader-dashboard.css',
 })
 export class ReaderDashboard {
   menu = [
     {text:"Τρέχουσα κατάσταση δανεισμού", link:'current-borrowed-books'},
-    {text:"Αναζήτηση βιβλίων", link:'search-books'}
+    {text:"Επιθυμητά", link: 'wish-list'},
+    {text:"Αναζήτηση βιβλίων", link:'search-books'},
+    {text:"Ενημέρωση στοιχείων χρήστη", link:'update-user'}
   ]
-
 }
