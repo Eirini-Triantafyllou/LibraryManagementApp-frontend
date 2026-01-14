13d65811495a6243f622a6b4ec18c2ecca08d4d0
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-14 10:52:20 +0200

    chore: update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
 
-import { LoginUser } from './components/login-user/login-user';
-import { Footer } from './components/footer/footer';
-import { ReaderDashboard } from './components/reader-dashboard/reader-dashboard';
-import { LibrarianDashboard } from './components/librarian-dashboard/librarian-dashboard';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { UserRole } from './shared/enums/user-role';
 import { DashboardService } from './shared/services/dashboard.service';
 import { AuthService } from './shared/services/auth.service';
 import { User } from './shared/interfaces/user';
 import { CommonModule } from '@angular/common';
-import { Navbar } from './components/navbar/navbar';
-import { UpdateUser } from './components/update-user/update-user';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterModule,
     CommonModule,
-    RouterLink,
-    RouterOutlet,
-    Navbar,
-    ReaderDashboard,
-    LibrarianDashboard,
-    LoginUser,
-    Footer,
-    UpdateUser
+    RouterOutlet
 ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -37,7 +24,5 @@ export class App {
 
 userRole: any;
 user: any;
-
-
-  
+ 
 }
