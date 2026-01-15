27222161cc6759302ff9b7e6f5bf355360a28736
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-15 20:18:54 +0200

    fix: update librarian-dashboard

--- src/app/components/librarian-dashboard/librarian-dashboard.ts
+++ src/app/components/librarian-dashboard/librarian-dashboard.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-librarian-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, Navbar, Footer],
   templateUrl: './librarian-dashboard.html',
   styleUrl: './librarian-dashboard.css',
 })
 export class LibrarianDashboard {
 
   menu = [
-    {text:"Αναζήτηση αναγνωστών", link:'search-readers'},
-    {text:"Αναζήτηση βιβλίων", link:'search-books'}
+    {text:"👤 Αναζήτηση αναγνωστών", link:'search-readers'},
+    {text:"🔍 Αναζήτηση βιβλίων", link:'search-books'},
+    {text:"📚  Επεξεργασία εγγραφών (εισαγωγή/ αφαίρεση βιβλίων)", link:'edit-books'}
   ]
-
 }
