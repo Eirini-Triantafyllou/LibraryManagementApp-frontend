dc7c66abf62d0f90a272d05313183e164263eae5
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-16 12:39:18 +0200

    feat: create admin dashboard component

---
+++ src/app/components/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, Navbar, Footer],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.css',
+})
+export class AdminDashboard {
+
+  menu = [
+    {text:"👤 Αναζήτηση αναγνωστών", link: 'search-readers'}, 
+    {text:"👤 Αναζήτηση εργαζόμενων", link: 'search-librarians'}
+  ]
+}
