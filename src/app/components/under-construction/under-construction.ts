66bb14ee334910f85639f7427d1910cd3a4a4fda
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-17 19:38:01 +0200

    feat: create under construction component

---
+++ src/app/components/under-construction/under-construction.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-under-construction',
+  standalone: true,
+  imports: [ CommonModule ],
+  templateUrl: './under-construction.html',
+  styleUrl: './under-construction.css',
+})
+export class UnderConstruction {
+
+}
